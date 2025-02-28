57bdc69e0db3113db8b7c6e96601c77013ccf596
yoneyy <yoneyy@163.com>
2025-03-01 07:38:26 +0800

    fix: Fixed the problem of importing files that cannot match the corresponding type in esm

--- scripts/build.js
+++ scripts/build.js
@@ -5,9 +5,11 @@ const { spawn } = require('child_process');
 const { stdout } = spawn('tsc', ['-p', 'tsconfig.json'], { cwd: process.cwd() });
 stdout.on('close', () => {
   const libraryPath = path.join(__dirname, '../lib/index.d.ts');
+  const esmLibraryPath = path.join(__dirname, '../lib/esm/index.d.ts');
   const code = fs
     .readFileSync(libraryPath, 'utf8')
     .replace('export default strdm;', '');
   const result = `${code}\rexport = strdm;`;
   fs.writeFileSync(libraryPath, result, { encoding: 'utf8' });
+  fs.writeFileSync(esmLibraryPath, result, { encoding: 'utf8' });
 });
